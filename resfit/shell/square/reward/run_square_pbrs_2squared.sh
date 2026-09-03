687c2f11f2c2aec10414c33a5f41fa6ea41313cf
YSL2683 <dbstjd55@seoultech.ac.kr>
2026-09-03 10:42:04 +0900

    Add torch_deterministic=true options to all square training scripts for reproducibility

--- resfit/shell/square/reward/run_square_pbrs_2squared.sh
+++ resfit/shell/square/reward/run_square_pbrs_2squared.sh
@@ -91,4 +91,5 @@ python3.10 resfit/rl_finetuning/scripts/train_residual_td3.py \
     base_policy_path="${BASE_POLICY_PATH}" \
     e2c_dir="${E2C_DIR}" \
     offline_data.name="${OFFLINE_DATA_DIR}" \
-    eval_interval_every_steps=2000
+    eval_interval_every_steps=2000 \
+    torch_deterministic=true
